fd90a8ffc43d35444977e2f47e932cec844ad867
dav-vid <villalobomunoz@icloud.com>
2024-11-12 11:37:14 -0500

    :(

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,12 @@
-/*const usuario = JSON.parse(localStorage.getItem('usuario')) || null;
-const button = document.getElementById('buttonSesion');
-const banner = document.getElementById('bienvenidoUsuario');
-console.log(button)
-if (usuario) {
-    console.log(usuario)
-    banner.innerHTML = 'Bienvenid@ ' + usuario.nombre + ' ' + usuario.apellido;
-    button.innerHTML = 'Cerrar Sesion'
-    button.classList.add("inicio-sesion")
-}
-button.addEvenListener('click', function (even) {
-    if (button.classList.contains('inicio-sesion')) {
-        button.classList.remove('inicio-sesion')
-        localStorage.removeItem('usuario')
-        banner.innerHTML = 'bienvenido'
-    }
-})*/
 
 const usuario = JSON.parse(localStorage.getItem('usuario')) || null;
 const button = document.getElementById('buttonSesion');
 const banner = document.getElementById('bienvenidoUsuario');
-console.log(button)
 if (usuario) {
-    console.log(usuario)
     banner.innerHTML = 'Bienvenid@ ' + usuario.nombre + ' ' + usuario.apellido;
     button.innerHTML = 'Cerrar Sesion'
     button.classList.add("inicio-sesion")
 }
-console.log(button)
 button.addEventListener('click', function (even) {
     if (button.classList.contains('inicio-sesion')) {
         button.classList.remove('inicio-sesion')
@@ -38,4 +18,46 @@ button.addEventListener('click', function (even) {
     }
 })
 
+
+
+const reponsive = document.querySelector("#responsive")
+const navbar = document.querySelector("#barnav")
+
+reponsive.addEventListener("click", () => {
+    navbar.classList.toggle("responsive")
+
+})
+
+//3 ventana modal
+const adquirir = document.querySelector("#adquirirmodal");
+const abrir = document.querySelector("#ventana")
+
+adquirir.addEventListener("click",() =>{
+    abrir.classList.add("abrir")
+} )
+const cerrar = document.querySelector("#x");
+cerrar.addEventListener("click", () => {
+    abrir.classList.remove("abrir")
+})
+//2 ventana modal
+const adquirir2 = document.querySelector("#adquirirmodal2");
+const abrir2 = document.querySelector("#ventana2")
+
+adquirir2.addEventListener("click",() =>{
+    abrir2.classList.add("abrir")
+} )
+const cerrar2 = document.querySelector("#x2");
+cerrar2.addEventListener("click", () => {
+    abrir2.classList.remove("abrir")
+})
+//1 ventana modañ
+const adquirir3 = document.querySelector("#adquirirmodal3");
+const abrir3 = document.querySelector("#ventana3")
+adquirir3.addEventListener("click",() =>{
+    abrir3.classList.add("abrir")
+} )
+const cerrar3 = document.querySelector("#x3");
+cerrar3.addEventListener("click", () => {
+    abrir3.classList.remove("abrir")
+})
 
